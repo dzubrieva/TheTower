0810e49bca8c44aa30f4a0f2a02bb631bc449c8b
dzubrieva <47629419+dzubrieva@users.noreply.github.com>
2019-02-14 14:59:15 +0200

    Update GameSettings.cs

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
 
-
 [CreateAssetMenu(fileName = "New Settings", menuName = "Settings")]
 public class GameSettings : ScriptableObject
 {
     [Header("Colors")]
     public Color failCylinderColor;
-
 
     [Header("Cylinder Values")]
     public float increaseDecreaseStep = 0.04f;
